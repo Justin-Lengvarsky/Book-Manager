bc1ea193807c62c440aa2ea388b39c79d532c5a8
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-10-19 11:56:20 -0400

    removed dbConnectionStr

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const PORT = 8000
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
-
-let dbConnectionStr = process.env.DB_STRING
+ 
 let db;
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true} )
+MongoClient.connect(process.env.DB_STRING, { useUnifiedTopology: true} )
   .then(client => {
     console.log('Connected to Database')
     db = client.db('book-manager')
